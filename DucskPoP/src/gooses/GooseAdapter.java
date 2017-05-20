0e2d794eb20ad9b2aee6018eb153342e1096c730
Ricardo Rafael Quintero Meza <iscrquinter@hotmail.com>
2017-05-20 17:39:07 -0600

    Agregando al ganso Observable ...

--- DucskPoP/src/gooses/GooseAdapter.java
+++ DucskPoP/src/gooses/GooseAdapter.java
@@ -1,16 +1,27 @@
 package gooses;
 
-import ducks.Quackable;
+import ducks.*;
 
 public class GooseAdapter implements Quackable {
 	Goose goose;
- 
+	Observable observable;
+
 	public GooseAdapter(Goose goose) {
 		this.goose = goose;
+		observable = new Observable(this);
 	}
  
 	public void quack() {
 		goose.honk();
+		notifyObservers();
+	}
+
+	public void registerObserver(Observer observer) {
+		observable.registerObserver(observer);
+	}
+
+	public void notifyObservers() {
+		observable.notifyObservers();
 	}
 
 	public String toString() {
