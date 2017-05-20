914ed8a1b3493d76e7872fa01737a8f15e538e24
Ricardo Rafael Quintero Meza <iscrquinter@hotmail.com>
2017-05-20 17:44:51 -0600

    Ejecutando el simulador con Observable+Observer ...

--- DucskPoP/src/app/DuckSimulator.java
+++ DucskPoP/src/app/DuckSimulator.java
@@ -4,31 +4,29 @@ import ducks.*;
 import gooses.*;
 
 public class DuckSimulator {
-
 	public static void main(String[] args) {
 		DuckSimulator simulator = new DuckSimulator();
 		AbstractDuckFactory duckFactory = new CountingDuckFactory();
  
 		simulator.simulate(duckFactory);
 	}
- 
+  
 	void simulate(AbstractDuckFactory duckFactory) {
+  
 		Quackable redheadDuck = duckFactory.createRedheadDuck();
 		Quackable duckCall = duckFactory.createDuckCall();
 		Quackable rubberDuck = duckFactory.createRubberDuck();
 		Quackable gooseDuck = new GooseAdapter(new Goose());
-
-		System.out.println("\nDuck Simulator: With Composite - Flocks");
-
+ 
 		Flock flockOfDucks = new Flock();
-
+ 
 		flockOfDucks.add(redheadDuck);
 		flockOfDucks.add(duckCall);
 		flockOfDucks.add(rubberDuck);
 		flockOfDucks.add(gooseDuck);
-
+ 
 		Flock flockOfMallards = new Flock();
-
+ 
 		Quackable mallardOne = duckFactory.createMallardDuck();
 		Quackable mallardTwo = duckFactory.createMallardDuck();
 		Quackable mallardThree = duckFactory.createMallardDuck();
@@ -41,17 +39,18 @@ public class DuckSimulator {
 
 		flockOfDucks.add(flockOfMallards);
 
-		System.out.println("\nDuck Simulator: Whole Flock Simulation");
-		simulate(flockOfDucks);
+		System.out.println("\nDuck Simulator: With Observer");
 
-		System.out.println("\nDuck Simulator: Mallard Flock Simulation");
-		simulate(flockOfMallards);
+		Quackologist quackologist = new Quackologist();
+		flockOfDucks.registerObserver(quackologist);
+
+		simulate(flockOfDucks);
 
 		System.out.println("\nThe ducks quacked " + 
 		                   QuackCounter.getQuacks() + 
 		                   " times");
 	}
-
+ 
 	void simulate(Quackable duck) {
 		duck.quack();
 	}
