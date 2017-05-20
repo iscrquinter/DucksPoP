e68983ec6eeba7ce4ae4d73ecfef79716596e751
Ricardo Rafael Quintero Meza <iscrquinter@hotmail.com>
2017-05-20 15:11:34 -0600

    Ejecutando simulador con AbstractFactory de Ducks ...

--- DucskPoP/src/app/DuckSimulator.java
+++ DucskPoP/src/app/DuckSimulator.java
@@ -6,28 +6,31 @@ import gooses.*;
 public class DuckSimulator {
 	public static void main(String[] args) {
 		DuckSimulator simulator = new DuckSimulator();
-		simulator.simulate();
+		AbstractDuckFactory duckFactory = new CountingDuckFactory();
+ 
+		simulator.simulate(duckFactory);
 	}
-
-	void simulate() {
-		Quackable mallardDuck = new QuackCounter(new MallardDuck());
-		Quackable redheadDuck = new QuackCounter(new RedheadDuck());
-		Quackable duckCall = new QuackCounter(new DuckCall());
-		Quackable rubberDuck = new QuackCounter(new RubberDuck());
+ 
+	void simulate(AbstractDuckFactory duckFactory) {
+		Quackable mallardDuck = duckFactory.createMallardDuck();
+		Quackable redheadDuck = duckFactory.createRedheadDuck();
+		Quackable duckCall = duckFactory.createDuckCall();
+		Quackable rubberDuck = duckFactory.createRubberDuck();
 		Quackable gooseDuck = new GooseAdapter(new Goose());
-
-		System.out.println("\nDuck Simulator: With Decorator");
-
+ 
+		System.out.println("\nDuck Simulator: With Abstract Factory");
+ 
 		simulate(mallardDuck);
 		simulate(redheadDuck);
 		simulate(duckCall);
 		simulate(rubberDuck);
 		simulate(gooseDuck);
-
+ 
 		System.out.println("The ducks quacked " + 
-		                   QuackCounter.getQuacks() + " times");
+		                   QuackCounter.getQuacks() + 
+		                   " times");
 	}
-
+ 
 	void simulate(Quackable duck) {
 		duck.quack();
 	}
