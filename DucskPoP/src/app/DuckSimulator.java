148306950848720a0619f956b5176fe16a0f50d1
Ricardo Rafael Quintero Meza <iscrquinter@hotmail.com>
2017-05-20 13:36:12 -0600

    Ejecutando simulador con Decorador de Ducks ...

--- DucskPoP/src/app/DuckSimulator.java
+++ DucskPoP/src/app/DuckSimulator.java
@@ -1,6 +1,5 @@
 package app;
 
-import ducks.MallardDuck;
 import ducks.*;
 import gooses.*;
 
@@ -11,21 +10,24 @@ public class DuckSimulator {
 	}
 
 	void simulate() {
-		Quackable mallardDuck = new MallardDuck();
-		Quackable redheadDuck = new RedheadDuck();
-		Quackable duckCall = new DuckCall();
-		Quackable rubberDuck = new RubberDuck();
+		Quackable mallardDuck = new QuackCounter(new MallardDuck());
+		Quackable redheadDuck = new QuackCounter(new RedheadDuck());
+		Quackable duckCall = new QuackCounter(new DuckCall());
+		Quackable rubberDuck = new QuackCounter(new RubberDuck());
 		Quackable gooseDuck = new GooseAdapter(new Goose());
- 
-		System.out.println("\nDuck Simulator : with Goose Adapter");
- 
+
+		System.out.println("\nDuck Simulator: With Decorator");
+
 		simulate(mallardDuck);
 		simulate(redheadDuck);
 		simulate(duckCall);
 		simulate(rubberDuck);
 		simulate(gooseDuck);
+
+		System.out.println("The ducks quacked " + 
+		                   QuackCounter.getQuacks() + " times");
 	}
- 
+
 	void simulate(Quackable duck) {
 		duck.quack();
 	}
