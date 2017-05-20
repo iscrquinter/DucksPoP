e8d4175e7b5d5b2908460cf796b683b7c3a908d7
Ricardo Rafael Quintero Meza <iscrquinter@hotmail.com>
2017-05-20 12:33:01 -0600

    Agregando adapatador Goose->Ducks ...

---
+++ DucskPoP/src/gooses/GooseAdapter.java
@@ -0,0 +1,19 @@
+package gooses;
+
+import ducks.Quackable;
+
+public class GooseAdapter implements Quackable {
+	Goose goose;
+ 
+	public GooseAdapter(Goose goose) {
+		this.goose = goose;
+	}
+ 
+	public void quack() {
+		goose.honk();
+	}
+
+	public String toString() {
+		return "Goose pretending to be a Duck";
+	}
+}
